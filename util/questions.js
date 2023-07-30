f4ddc157460524922241cb0ed1744ef2224e43c8
jalajcodes <jalaj@mail.com>
2023-07-30 10:08:40 +0530

    chore: add questions

---
+++ util/questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+  {
+    question: "Which team was the 2015-2016 NBA Champions?",
+    options: [
+      "Golden State Warriors",
+      "Cleveland Cavaliers",
+      "Toronto Raptors",
+      "Oklahoma City Thunders",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "What continent is the country Lesotho in?",
+    options: ["Asia", "Africa", "South America", "Europe"],
+    correctAnswer: 2,
+  },
+  {
+    question:
+      "How do you judge what should be added in the next version of the app?",
+    options: [
+      "Data Analysis",
+      "User's Feedback",
+      "Make a Questionary",
+      "Personal Feeling",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "Only one country in the world starts with the letter Q.",
+    options: ["True", "False", "I don't know", "I don't care"],
+    correctAnswer: 2,
+  },
+  {
+    question: "What country has a horizontal bicolor red and white flag?",
+    options: ["Bahrain", "Monaco", "Malta", "Liechenstein"],
+    correctAnswer: 2,
+  },
+];
